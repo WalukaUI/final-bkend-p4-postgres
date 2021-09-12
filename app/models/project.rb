243f983c8e7538185b911753f45be4c3745b960e
WalukaUI <cwaluka@yahoo.com>
2021-09-12 13:54:26 -0700

    table relationship created

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ApplicationRecord
+    has_many :employees, through: :project_employees
 end
--- app/models/project_employee.rb
+++ app/models/project_employee.rb
@@ -1,2 +1,4 @@
 class ProjectEmployee < ApplicationRecord
+    belongs_to :employees
+    belongs_to :projects
 end
