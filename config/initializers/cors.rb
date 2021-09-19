1e170625dfa3705c05e965fe15861d19cb8d1af1
WalukaUI <cwaluka@yahoo.com>
2021-09-19 11:34:55 -0700

    cors.rb origin changed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3006'
+    # origins 'http://localhost:3006'
+    
+    origins 'https://tnt-project-manager.netlify.app'
 
     resource '*',
       headers: :any,
