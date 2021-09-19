bd487b077369c45f136a89746f1eaaa8c908acec
WalukaUI <cwaluka@yahoo.com>
2021-09-19 12:05:16 -0700

    cors crediantilas changed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      credentials: true,
+      # credentials: true,
+      credentials: false,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
