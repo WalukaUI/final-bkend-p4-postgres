c8b5cc6c818b01a52c01cc0185883c10c7c24cc0
WalukaUI <cwaluka@yahoo.com>
2021-09-19 20:19:39 -0700

    cors edited to localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #origins 'https://localhost:3000'
-    
-    origins 'https://tnt-project-manager.netlify.app'
+    #origins 'https://tnt-project-manager.netlify.app'
+  origins 'https://localhost:3000'
 
     resource '*',
       headers: :any,
