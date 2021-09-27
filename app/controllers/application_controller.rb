1a71667b4acaff10df98d15a41d5ab236cac6581
WalukaUI <cwaluka@yahoo.com>
2021-09-27 01:05:33 -0700

    j

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+
     include ActionController::Cookies
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
@@ -8,7 +9,6 @@ class ApplicationController < ActionController::API
 
     def authorize
         @current_user = User.find_by(id: session[:user_id])
-    
         render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
     end
 
