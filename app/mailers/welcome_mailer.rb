447bf53f68a3a801a9a5ee7990ef8a4951482787
WalukaUI <cwaluka@yahoo.com>
2022-02-02 03:55:35 -0800

    mail successfully preveving

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,8 +1,8 @@
 class WelcomeMailer < ApplicationMailer
-    # default from: 'cwaluka@gmail.com'
+    default from: 'cwaluka@gmail.com'
 
     def welcome_email
-      @user = "waluka"
+      @user = params[:user]
       @url  = 'http://example.com/login'
       mail(to: "cwaluka@yahoo.com", subject: 'Welcome to My Awesome Site')
     end
