61f6a0b9d14c730e066a2cb031500bbce77db7c2
WalukaUI <cwaluka@yahoo.com>
2022-02-03 03:02:02 -0800

    added @vnumber to email

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,6 +1,7 @@
 class WelcomeMailer < ApplicationMailer
-    def welcome_email(newuser)
+    def welcome_email(newuser, verificationnum)
       @user = newuser
+      @vnumber=verificationnum
       @url  = 'https://tnt-project-manager.netlify.app'
       mail(to: @user.email, subject: 'Welcome to T&T Com.')
     end
