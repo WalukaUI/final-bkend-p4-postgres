f67416e48213bef5ffd55b2529b5921e99c14ac8
WalukaUI <cwaluka@yahoo.com>
2022-02-06 14:14:59 -0800

    delete mailer addded

--- app/mailers/delete_employee_mailer.rb
+++ app/mailers/delete_employee_mailer.rb
@@ -1,8 +1,8 @@
 class DeleteEmployeeMailer < ApplicationMailer
     def delete_employee(emp)
-        userdetails=User.find(session[:user_id])
+        @userdetails=User.find(session[:user_id])
         @emp = emp
-        mail(to: "cwaluka@yahoo.com", subject: 'Data Changed in your T&T account.')
+        mail(to: @userdetails.email, subject: 'Data Changed in your T&T account.')
       end
 end
 
